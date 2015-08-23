0cc4f6e898ac6ae8da33b597252d63edf722beed
Carlos Moreno <carlosandresmorenovelez@gmail.com>
2015-08-23 17:37:06 -0500

    Update urls.py

--- sitiolectura/urls.py
+++ sitiolectura/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    url(r'^$', RedirectView.as_view(url='/biblioteca/'), name='index'),#Esta linea me redirige de la raiz (localhost:8000) a sitiolectura (localhost:8000/sitiolectura), en dicho folder debra de haber una url que coincida con el patron
+    url(r'^$', RedirectView.as_view(url='/sitiolectura/'), name='index'),#Esta linea me redirige de la raiz (localhost:8000) a sitiolectura (localhost:8000/sitiolectura), en dicho folder debra de haber una url que coincida con el patron
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sitiolectura/', include('biblioteca.urls', namespace='sitio')),
